cc8ed4602f5d3d05c5839e77bd47d0656f2041e1
timothynodes <116788133+timothynodes@users.noreply.github.com>
2024-11-05 20:56:52 +0800

    Update ocm.sh OCM_VERSION="24.11.05.20.54"
    OCM_VERSION="24.11.05.20.54"

--- ocm.sh
+++ ocm.sh
@@ -1,26 +1,31 @@
 #!/usr/bin/env bash
 
 # variable
-OCM_VERSION="24.11.05.20.08"
+OCM_VERSION="24.11.05.20.54"
 
-    case "$num" in
+update_ocm_script() {
+cd ~ && rm -rf ocm.sh && wget https://raw.githubusercontent.com/timothynodes/timothynodes/refs/heads/main/ocm.sh  && chmod u+x ocm.sh && ./ocm.sh
+echo -e "The script has been updated to the latest version!" && exit 0
+}
+
+select_item() {
+    clear
+    echo "ocm.sh version 24.11.05.20.54"
+    echo "1. Update ocm.sh"
+    echo "0. Exit"
+    read -rp "Select item:" item
+    case "$item" in
     1)
-        update_shell
-        ;;
-    20)
-        new_account
+        update_ocm_script
         ;;
     0)
         exit 1
         ;;
     *)
         clear
-        if [ "$LANGUAGE" == "cn" ]; then
-            red "请输入正确数字 (0-20)"
-        else
-            red "Please enter the correct number (0-20)"
-        fi
         sleep 1s
-        start_menu
+        select_item
         ;;
     esac
+}
+select_item
