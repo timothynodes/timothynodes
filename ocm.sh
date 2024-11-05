80147ecc0571a8885c5a03c0bd22c51b3ce643c6
timothynodes <116788133+timothynodes@users.noreply.github.com>
2024-11-05 21:25:31 +0800

    Update ocm.sh OCM_VERSION="24.11.05.21.25"
    OCM_VERSION="24.11.05.21.25"

--- ocm.sh
+++ ocm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # variable
-OCM_VERSION="24.11.05.21.13"
+OCM_VERSION="24.11.05.21.25"
 
 update_ocm_script() {
 cd ~ && rm -rf ocm.sh && wget https://raw.githubusercontent.com/timothynodes/timothynodes/refs/heads/main/ocm.sh
@@ -11,13 +11,17 @@ chmod u+x ocm.sh && ./ocm.sh
 }
 
 select_item() {
-    echo "ocm.sh version 24.11.05.21.13"
+    echo "ocm.sh version 24.11.05.21.25"
     echo "1. Update ocm.sh"
+    echo "2. Autonomys(Subspace)(OS:Ubuntu desktop)
     echo "0. Exit"
     read -rp "Select item:" item
     case "$item" in
     1)
         update_ocm_script
+        ;;
+    2)
+        cd ~ && rm -rf autonomys.sh && wget https://raw.githubusercontent.com/timothynodes/timothynodes/refs/heads/main/autonomys.sh && chmod u+x autonomys.sh && ./autonomys.sh
         ;;
     0)
         exit 1
