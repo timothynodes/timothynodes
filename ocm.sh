e85c35dcf2d8d717f425dfef7591a3d1b71f6140
timothynodes <116788133+timothynodes@users.noreply.github.com>
2024-11-05 21:05:10 +0800

    Update ocm.sh OCM_VERSION="24.11.05.21.05"
    OCM_VERSION="24.11.05.21.05"

--- ocm.sh
+++ ocm.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
 # variable
-OCM_VERSION="24.11.05.20.58"
+OCM_VERSION="24.11.05.21.05"
 
 update_ocm_script() {
-cd ~ && rm -rf ocm.sh && wget https://raw.githubusercontent.com/timothynodes/timothynodes/refs/heads/main/ocm.sh  && chmod u+x ocm.sh && ./ocm.sh
-echo -e "The script has been updated to the latest version!" && exit 0
+cd ~ && rm -rf ocm.sh && wget https://raw.githubusercontent.com/timothynodes/timothynodes/refs/heads/main/ocm.sh
+echo -e "The script has been updated to the latest version!"
+sleep 1s
+chmod u+x ocm.sh && ./ocm.sh
 }
 
 select_item() {
     clear
-    echo "ocm.sh version 24.11.05.20.58"
+    echo "ocm.sh version 24.11.05.21.05"
     echo "1. Update ocm.sh"
     echo "0. Exit"
     read -rp "Select item:" item
