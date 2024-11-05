341afe0fd3af5c21bb35f3b4674005d814bd16b3
timothynodes <116788133+timothynodes@users.noreply.github.com>
2024-11-05 23:12:50 +0800

    Update ocm.sh OCM_VERSION="24.11.05.23.12"
    OCM_VERSION="24.11.05.23.12"

--- ocm.sh
+++ ocm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # variable
-OCM_VERSION="24.11.05.22.10"
+OCM_VERSION="24.11.05.23.12"
 
 update_ocm_script() {
 cd ~ && rm -rf ocm.sh && wget https://raw.githubusercontent.com/timothynodes/timothynodes/refs/heads/main/ocm.sh
@@ -11,7 +11,8 @@ chmod u+x ocm.sh && ./ocm.sh
 }
 
 select_item() {
-    echo "ocm.sh version 24.11.05.22.10"
+    echo "ocm.sh version 24.11.05.23.12"
+    echo "https://t.me/ocm_g"
     echo "1. Update ocm.sh"
     echo "2. Autonomys(Subspace)(OS:Ubuntu desktop)"
     echo "0. Exit"
