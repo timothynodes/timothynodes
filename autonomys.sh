269b76df9f20a0dcc3d4226e11d8c5ab071ce054
timothynodes <116788133+timothynodes@users.noreply.github.com>
2024-11-07 07:15:16 +0800

    Update autonomys.sh AUTONOMYS_VERSION="24.11.06.23.15"
    AUTONOMYS_VERSION="24.11.06.23.15"

--- autonomys.sh
+++ autonomys.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
 # variable
-AUTONOMYS_VERSION="24.11.05.23.16"
+AUTONOMYS_VERSION="24.11.06.23.15"
 AUTONOMYS_DIR="autonomys"
 
 space_acres() {
 cd ~
 mkdir -p "$AUTONOMYS_DIR"
 cd "$AUTONOMYS_DIR"
-if [ ! -f "space-acres-0.1.38-x86_64.AppImage" ]; then  
-  wget https://github.com/autonomys/space-acres/releases/download/0.1.38/space-acres-0.1.38-x86_64.AppImage
-  chmod u+x space-acres-0.1.38-x86_64.AppImage
-  ./space-acres-0.1.38-x86_64.AppImage  --appimage-extract
+if [ ! -f "space-acres-0.2.0-x86_64.AppImage" ]; then  
+  wget https://ghp.ci/https://github.com/autonomys/space-acres/releases/download/0.2.0/space-acres-0.2.0-x86_64.AppImage
+  chmod u+x space-acres-0.2.0-x86_64.AppImage
+  ./space-acres-0.2.0-x86_64.AppImage  --appimage-extract
 fi
 ./squashfs-root/AppRun
 }
 
 select_item() {
-    echo "1. space-acres-0.1.38-x86_64.AppImage"
+    echo "1. space-acres-0.2.0-x86_64.AppImage"
     echo "0. Exit"
     read -rp "Select item:" item
     case "$item" in
