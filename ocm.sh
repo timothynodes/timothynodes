c6d2201d634b4cb09cc065d51f48a5d5aff1e125
timothynodes <116788133+timothynodes@users.noreply.github.com>
2024-11-06 23:16:19 +0800

    Update ocm.sh OCM_VERSION="24.11.06.23.15"
    OCM_VERSION="24.11.06.23.15"

--- ocm.sh
+++ ocm.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 
 # variable
-OCM_VERSION="24.11.06.23.04"
+OCM_VERSION="24.11.06.23.15"
 
 update_ocm_script() {
 # cd ~ && rm -rf ocm.sh && wget https://raw.githubusercontent.com/timothynodes/timothynodes/refs/heads/main/ocm.sh && chmod u+x ocm.sh && ./ocm.sh
 cd ~ && rm -rf ocm.sh_new && wget https://raw.githubusercontent.com/timothynodes/timothynodes/refs/heads/main/ocm.sh -O ocm.sh_new
 if [ ! -f "~/ocm.sh_new" ]; then
-  rm -rf ocm.sh && mv ocm.sh_new ocm.sh
+  rm -rf ocm.sh && mv ocm.sh_new ocm.sh && chmod u+x ocm.sh
 fi
+echo -e "=================================================="
 echo -e "The script has been updated to the latest version!"
-sleep 2s
-chmod u+x ocm.sh && ./ocm.sh
+echo -e "=================================================="
+sleep 1s
+./ocm.sh
 }
 
 select_item() {
-    echo "ocm.sh version 24.11.06.23.04"
+    echo "ocm.sh version 24.11.06.23.15"
     echo "https://t.me/ocm_g"
-    update_ocm_script
     echo "1. Update ocm.sh"
     echo "2. Autonomys(Subspace)(OS:Ubuntu desktop)"
     echo "0. Exit"
