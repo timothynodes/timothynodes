06d05c659cd4975fa2acf3111cd6b4a8991f5a91
timothynodes <116788133+timothynodes@users.noreply.github.com>
2024-11-08 19:23:46 +0800

    Update autonomys.sh AUTONOMYS_VERSION="24.11.08.11.23"
    AUTONOMYS_VERSION="24.11.08.11.23"

--- autonomys.sh
+++ autonomys.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # variable
-AUTONOMYS_VERSION="24.11.08.11.20"
+AUTONOMYS_VERSION="24.11.08.11.23"
 AUTONOMYS_DIR="autonomys"
 
 space_acres() {
@@ -9,10 +9,10 @@ cd ~
 mkdir -p "$AUTONOMYS_DIR"
 cd "$AUTONOMYS_DIR"
 if [ ! -f "space-acres-0.2.0-x86_64.AppImage" ]; then
-  # echo "deb http://cz.archive.ubuntu.com/ubuntu jammy main" | sudo tee -a /etc/apt/sources.list \
-  && sudo apt update \
-  && sudo apt install libc6 -y \
-  && sudo apt install libpango-1.0-0 -y
+  # echo "deb http://cz.archive.ubuntu.com/ubuntu jammy main" | sudo tee -a /etc/apt/sources.list && \
+  sudo apt update && \
+  sudo apt install libc6 -y && \
+  sudo apt install libpango-1.0-0 -y
   wget https://ghp.ci/https://github.com/autonomys/space-acres/releases/download/0.2.0/space-acres-0.2.0-x86_64.AppImage
   chmod u+x space-acres-0.2.0-x86_64.AppImage
   ./space-acres-0.2.0-x86_64.AppImage  --appimage-extract
