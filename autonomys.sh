7d0dd97e373d10c593cc3a80ffef2f6d0c1160e6
timothynodes <116788133+timothynodes@users.noreply.github.com>
2024-11-13 20:20:11 +0800

    Update autonomys.sh AUTONOMYS_VERSION="24.11.13.12.10"
    AUTONOMYS_VERSION="24.11.13.12.10"

--- autonomys.sh
+++ autonomys.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # variable
-AUTONOMYS_VERSION="24.11.11.01.50"
-AUTONOMYS_DIR="autonomys"
+AUTONOMYS_VERSION="24.11.13.12.10"
+AUTONOMYS_DIR="ocm/autonomys"
 NODE_DATA_PATH=""
 YOUR_NODE_NAME=""
 WALLET_ADDRESS=""
@@ -43,16 +43,17 @@ fi
 
 space_node() {
 change_dir
-if [ ! -f "subspace-node-ubuntu-x86_64-skylake-mainnet-2024-nov-06" ]; then
+if [ ! -f "subspace-node-ubuntu-x86_64-skylake-mainnet-2024-nov-13" ]; then
   # apt_upgrade
-  wget https://ghp.ci/https://github.com/autonomys/subspace/releases/download/mainnet-2024-nov-06/subspace-node-ubuntu-x86_64-skylake-mainnet-2024-nov-06
+  wget https://ghp.ci/https://github.com/autonomys/subspace/releases/download/mainnet-2024-nov-13/subspace-node-ubuntu-x86_64-skylake-mainnet-2024-nov-13
   chmod_wget
+  ln -s subspace-node-ubuntu-x86_64-skylake-mainnet-2024-nov-13 subspace-node
 fi
 if [ -z "$PATH_TO_NODE" ]; then
-  echo "ex : /mnt"
+  echo "ex : /node"
   read -rp "PATH_TO_NODE : " PATH_TO_NODE
 fi
-./subspace-node-ubuntu-x86_64-skylake-mainnet-2024-nov-06 run \
+./subspace-node run \
   --chain mainnet \
   --blocks-pruning 256 \
   --state-pruning archive-canonical \
@@ -66,10 +67,11 @@ fi
 
 space_farmer() {
 change_dir
-if [ ! -f "subspace-farmer-ubuntu-x86_64-skylake-mainnet-2024-nov-06" ]; then
+if [ ! -f "subspace-farmer-ubuntu-x86_64-skylake-mainnet-2024-nov-13" ]; then
   # apt_upgrade
-  wget https://ghp.ci/https://github.com/autonomys/subspace/releases/download/mainnet-2024-nov-06/subspace-farmer-ubuntu-x86_64-skylake-mainnet-2024-nov-06
+  wget https://ghp.ci/https://github.com/autonomys/subspace/releases/download/mainnet-2024-nov-13/subspace-farmer-ubuntu-x86_64-skylake-mainnet-2024-nov-13
   chmod_wget
+  ln -s subspace-farmer-ubuntu-x86_64-skylake-mainnet-2024-nov-13 subspace-farmer
 fi
 if [ -z "$WALLET_ADDRESS" ]; then
   read -rp "WALLET_ADDRESS : " WALLET_ADDRESS
@@ -79,14 +81,14 @@ if [ -z "$NODE_RPC_URL" ]; then
   read -rp "NODE_RPC_URL : " NODE_RPC_URL
 fi
 if [ -z "$PATH_TO_FARM" ]; then
-  echo "ex : /mnt"
+  echo "ex : /farmer"
   read -rp "PATH_TO_FARM : " PATH_TO_FARM
 fi
 if [ -z "$PLOT_SIZE" ]; then
   echo "ex : 2G"
   read -rp "PLOT_SIZE : " PLOT_SIZE
 fi
-./subspace-farmer-ubuntu-x86_64-skylake-mainnet-2024-nov-06 farm \
+./subspace-farmer farm \
   --reward-address $WALLET_ADDRESS \
   --node-rpc-url $NODE_RPC_URL \
   --listen-on /ip4/0.0.0.0/tcp/30533 \
@@ -95,7 +97,7 @@ fi
 
 select_item() {
   echo "1. Autonomys(Ubuntu 24.04)space-acres       0.2.0-x86_64.AppImage"
-  echo "2. Autonomys(Linux CLI)subspace-node        2024-nov-06"
+  echo "2. Autonomys(Linux CLI)subspace-node        2024-nov-13(background)"
   echo "3. Autonomys(Linux CLI)subspace-farmer      2024-nov-06" 
   echo "0. Exit"
   read -rp "Select item:" item
